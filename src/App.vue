782c29deaac1302badd7557732ae35a4a42c3499
arup-paul <aruppaulctg@gmail.com>
2021-02-08 12:55:10 -0800

    countdown timer app

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <Counter
+    :year="2021"
+    :month="1"
+    :date="9"
+    :hour="11"
+    :minute="2"
+    :second="59"
+    :millisecond="2"
+
+  />
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Counter from './components/Counter.vue';
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+   <div>
+       <h1>Hello world</h1>
+       <Counter/>
+   </div>
+</template>
+
+
+
+<script>
+import Counter from "./Components/Counter";
+export default {
+    components:{
+      
+    }
+}
+</script>
+
+
+<style>
+
+</style>
